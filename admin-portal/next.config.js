e12f4317b2b8cabbeffaf79b893439750ff131a8
kclaudeeager <claudekwizera003@gmail.com>
2025-07-09 15:29:51 +0200

    Handling export

--- admin-portal/next.config.js
+++ admin-portal/next.config.js
@@ -1,13 +1,15 @@
+const backendUrl = process.env.NEXT_PUBLIC_API_URL;
+
 module.exports = {
   async rewrites() {
     return [
       {
         source: '/api/export_lab_pdf',
-        destination: 'http://localhost:3001/api/export_lab_pdf',
+        destination: `${backendUrl}/api/export_lab_pdf`,
       },
       {
         source: '/api/export_lab_docx',
-        destination: 'http://localhost:3001/api/export_lab_docx',
+        destination: `${backendUrl}/api/export_lab_docx`,
       },
     ];
   },
